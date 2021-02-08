a9e920b19a7de39f2c4be7b6814f549f1593dad2
sean <toosean@gmail.com>
2021-02-09 03:08:04 +0800

    step

--- src/Fetchers/Abstractions/FetcherServiceCollectionExtensions.cs
+++ src/Fetchers/Abstractions/FetcherServiceCollectionExtensions.cs
@@ -4,6 +4,7 @@ using ChinaPublicCalendarGenerator.Fetchers.Abstraction;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 
 namespace Microsoft.Extensions.DependencyInjection
@@ -11,14 +12,21 @@ namespace Microsoft.Extensions.DependencyInjection
     public static class FetcherServiceCollectionExtensions
     {
         public static IServiceCollection AddFetchers(this IServiceCollection services)
+            => FetcherServiceCollectionExtensions.AddFetchers(services, typeof(FetcherServiceCollectionExtensions).Assembly);
+
+        public static IServiceCollection AddFetchers(this IServiceCollection services, params Assembly[] assemblies)
         {
             var typeOfIFetcher = typeof(IFetcher);
+            var typeOfFetchShortNameAttribute = typeof(FetchShortNameAttribute);
 
-            var fetchTypeCollection = new FetcherTypeCollection(typeof(FetcherServiceCollectionExtensions).Assembly.DefinedTypes
-                .Where(w => w.ImplementedInterfaces.Contains(typeOfIFetcher))
-                .Where(w => !w.IsInterface)
-                .Where(w => !w.IsAbstract)
-                .ToList());
+            var fetchTypeCollection = new FetcherTypeCollection(
+                    assemblies.Select(s => s.DefinedTypes)
+                        .SelectMany(s => s)
+                        .Where(w => w.CustomAttributes.Any(a => a.AttributeType == typeOfFetchShortNameAttribute))
+                        .Where(w => w.ImplementedInterfaces.Contains(typeOfIFetcher))
+                        .Where(w => !w.IsInterface)
+                        .Where(w => !w.IsAbstract)
+                        .ToList());
 
             services.AddSingleton(fetchTypeCollection);
 
